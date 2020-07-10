50b6930a9c2ad2cbc637aef29b61cb21b24a9a80
Warda Ayaz <wayaz107@gmail.com>
2020-07-10 20:38:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,5 +9,11 @@ def countdown (integer)
 end
 
 def countdown_with_sleep(integer)
-  sleep(5)
+  while integer > 0
+    puts "#{integer} SECOND(S)!" 
+     sleep(1)
+    integer -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
